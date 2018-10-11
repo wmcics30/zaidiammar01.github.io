6995fef3ba8df2b6f7ff79ae2d331c67776fa598
zaidiammar01 <43014388+zaidiammar01@users.noreply.github.com>
2018-10-11 14:16:47 -0600

    titlescreen

--- UltimateTrafficLight/sketch.js
+++ UltimateTrafficLight/sketch.js
@@ -4,8 +4,8 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let y;
-let dy;
+let y, y2;
+let dy, dy2, dx;
 let x;
 let stateTitleScreen;
 let state;
@@ -14,9 +14,9 @@ let lastTimeSwitchedColor = 0;
 let lastTimeSwitchedColorX = 0;
 
 
-let RED_LIGHT_DURATION = 4000;
-let GREEN_LIGHT_DURATION = 3000;
-let YELLOW_LIGHT_DURATION = 1000;
+let RED_LIGHT_DURATION = 8000;
+let GREEN_LIGHT_DURATION = 6000;
+let YELLOW_LIGHT_DURATION = 2000;
 let elapsedTime;
 let elapsedTimeX;
 
@@ -25,20 +25,49 @@ function setup() {
   stateTitleScreen = 1;
   state = 1;
   stateX = 2;
-  x = width /2;
+  x = width /2 + 10;
   y = height;
+  y2 = height + 200;
   dy = -2;
+  dy2 = -1.9;
+  dx = 2;
 }
 
 function draw() {
-  background(100);
-  drawIntersection();
-  drawRect();
-  checkForStateChange();
-  checkForStateChangeX();
-  displayCorrectLight();
-  displayCorrectLightX();
-  displayCarY();
+  titleScreen();
+  runSimulation();
+}
+
+function titleScreen(){
+  if (mouseIsPressed){
+    stateTitleScreen = 2;
+  }
+  else{
+    background(0, 150, 0);
+    fill(150, 200, 255);
+    rect(0, 0, width, height/2);
+    textSize(75);
+    fill(255);
+    textStyle(NORMAL);
+    text("Ultimate Traffic Light Simulator!!!", width/5, height/6);
+    textStyle(ITALIC);
+    text("Click to Run Simulation", width/2.5, height/1.2);
+  }
+}
+
+function runSimulation(){
+  if (stateTitleScreen === 2){
+    background(100);
+    drawIntersection();
+    drawRect();
+    checkForStateChange();
+    checkForStateChangeX();
+    displayCorrectLight();
+    displayCorrectLightX();
+    displayCarY();
+    displayCarY2();
+    displayCarX1();
+  }
 
 }
 
@@ -88,11 +117,47 @@ function displayCarY(){
   if (y <= 0 - 80){
     y = height;
   }
+  else if (y - y2 <= 80 && y > y2){
+    y;
+  }
   else if (y >= height/2 + 70 && y <= height/2 + 90 && state === 1){
     y;
   }
   else{
     y += dy;
+  }
+}
+
+function displayCarY2(){
+  fill(20);
+  rect(x,y2,30,60);
+  if (y2 <= 0 - 80){
+    y2 = height;
+  }
+  else if (y2 - y <= 80 && y2 > y){
+    y2;
+  }
+  else if (y2 >= height/2 + 70 && y2 <= height/2 + 90 && state === 1){
+    y2;
+  }
+  else{
+    y2 += dy2;
+  }
+}
+
+function displayCarX1(){
+  fill(200);
+  x = 0;
+  y = height /2 + 10;
+  rect(x,y,60,30);
+  if (x >= width + 80){
+    x = width;
+  }
+  else if (x >= width/2 - 70 && y2 <= width/2 - 90 && state === 1){
+    x;
+  }
+  else{
+    x += dx;
   }
 }
 
@@ -201,3 +266,32 @@ function drawRect() {
   rect(width/2 - 140, height/2 + 100, 60, 20, 1);
   rect(width/2 + 80, height/2 - 100, 60, 20, 1);
 }
+
+// let thisCar = {
+//     x: 100,
+//     y: 50,
+//     dx: 0,
+//     dy: 5,
+// };
+// undefined
+// thisCar.x
+// 100
+// let otherCar = {
+//     x: 200,
+//     y: 450,
+//     dx: 1,
+//     dy: 0,
+// };
+// undefined
+// carArray = []
+// []
+// carArray.push(thisCar)
+// 1
+// carArray
+// [{…}]0: dx: 0dy: 5x: 100y: 50__proto__: Objectlength: 1__proto__: Array(0)
+// carArray.push(otherCar)
+// 2
+// carArray[0].x
+// 100
+// carArray[1].x
+// 200
